96ba99805e3b449ad2fd291a92251711f2e2f9c3
Baptiste C <baptiste.chauviere@lyceeastier.com>
2026-07-26 14:01:58 +0200

    Add banner and refactor HTTP server launch function
    Added a banner function to display a welcome message and refactored the HTTP server launch function for better readability.

--- src/main.c
+++ src/main.c
@@ -4,14 +4,23 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 
-int main() {
+void banner() {
+    printf("\n");
+    printf(" _   _ _____ _____ ____    ____                           \n");
+    printf("| | | |_   _|_   _|  _ \\  / ___|  ___ _ ____   _____ _ __ \n");
+    printf("| |_| | | |   | | | |_) | \\___ \\ / _ \\ '__\\ \\ / / _ \\ '__|\n");
+    printf("|  _  | | |   | | |  __/   ___) |  __/ |   \\ V /  __/ |   \n");
+    printf("|_| |_| |_|   |_| |_|     |____/ \\___|_|    \\_/ \\___|_|   \n");
+    printf("\n");
+}
+
+void launchHttpServer() {
     int server, client;
     struct sockaddr_in addr;
     socklen_t addr_len = sizeof(addr);
     char buffer[1024] = {0};
 
     server = socket(AF_INET, SOCK_STREAM, 0);
-
     int opt = 1;
     setsockopt(server, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
 
@@ -22,25 +31,41 @@ int main() {
     bind(server, (struct sockaddr *)&addr, sizeof(addr));
     listen(server, 5);
 
-    printf("Listening on port 8080...\n");
+    while (1) {
+        printf("Listening on port 8080...\n");
+        client = accept(server, (struct sockaddr *)&addr, &addr_len);
 
-    client = accept(server, (struct sockaddr *)&addr, &addr_len);
+        int n = read(client, buffer, sizeof(buffer) - 1);
+        if (n > 0) buffer[n] = '\0';
+        printf("Received: %s\n", buffer);
 
-    int n = read(client, buffer, sizeof(buffer) - 1);
-    buffer[n] = '\0';
-    printf("Received: %s\n", buffer);
+        FILE *file = fopen("file.txt", "r");
+        char body[4096] = {0};
 
-    FILE *file;
-    file = fopen("file.txt", "r");
+        size_t total = fread(body, 1, sizeof(body) - 1, file);
+        body[total] = '\0';
+        fclose(file);
+     
+        char response[4500];
+        int len = snprintf(response, sizeof(response),
+            "HTTP/1.1 200 OK\r\n"
+            "Content-Type: text/plain\r\n"
+            "Content-Length: %zu\r\n"
+            "Connection: close\r\n"
+            "\r\n"
+            "%s",
+            strlen(body), body);
 
-    char response[100];
+        write(client, response, len);
+        close(client);
+    }
 
-    // fgets isnt the good way to do this coz it'll only return the 1st line..
-    fgets(response, 100, file);
-
-    write(client, response, strlen(response));
-
-    close(client);
     close(server);
+}
+
+int main() {
+    banner();
+    launchHttpServer();
+
     return 0;
 }
